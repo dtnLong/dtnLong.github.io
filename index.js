851dd1e1f767ae76ce46687b6a2178cd69e8b39b
dtnLong <49140143+dtnLong@users.noreply.github.com>
2022-01-17 01:08:58 +0700

    deploy

--- index.js
+++
@@ -1,4 +0,0 @@
-window.onload = () => {
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    window.location.replace("http://ecommerce-env.eba-in9ynkdf.ap-southeast-1.elasticbeanstalk.com/?" + urlSearchParams.toString());
-}
