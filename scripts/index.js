d50a595e9c005cbf5fbf22eba3a78b2435b8cd0a
dtnLong <49140143+dtnLong@users.noreply.github.com>
2022-05-19 22:18:55 +0700

    Test

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 window.onload = () => {
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    let str = "";
-    if (urlSearchParams.toString() != "") {
-        str = str + "?" + urlSearchParams.toString();
-    }
-    window.location.replace("http://ecommerce-env.eba-in9ynkdf.ap-southeast-1.elasticbeanstalk.com/" + str);
+    // const urlSearchParams = new URLSearchParams(window.location.search);
+    // let str = "";
+    // if (urlSearchParams.toString() != "") {
+    //     str = str + "?" + urlSearchParams.toString();
+    // }
+    // window.location.replace("http://ecommerce-env.eba-in9ynkdf.ap-southeast-1.elasticbeanstalk.com/" + str);
 }
