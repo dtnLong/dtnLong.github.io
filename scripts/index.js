eb1e7c202beebd00f816196972610de5a450ac2a
dtnLong <49140143+dtnLong@users.noreply.github.com>
2022-05-19 22:47:11 +0700

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,3 @@
 window.onload = () => {
-    // const urlSearchParams = new URLSearchParams(window.location.search);
-    // let str = "";
-    // if (urlSearchParams.toString() != "") {
-    //     str = str + "?" + urlSearchParams.toString();
-    // }
-    // window.location.replace("http://ecommerce-env.eba-in9ynkdf.ap-southeast-1.elasticbeanstalk.com/" + str);
+    window.location.replace("https://dtnlong.github.io/aboutus/aboutus.html");
 }
